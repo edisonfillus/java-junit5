805b49bb8bfc10381f0c180f4e588eddfb4ce6e5
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-03-06 11:29:50 -0300

    Initial test, Maven configuration to use JUnit 5, JaCoCo and Sonar

---
+++ src/main/java/org/project/example/Palindrome.java
@@ -0,0 +1,18 @@
+package org.project.example;
+
+public class Palindrome {
+
+	public boolean isPalindrome(String phrase) {
+
+		String filteredPhrase = phrase.toUpperCase().replace(" ", "").replace("-", "").replace("–", "").replace(",", "");
+
+		for (int i = 0; i < filteredPhrase.length(); i++) {
+
+			if (filteredPhrase.charAt(i) != filteredPhrase.charAt(filteredPhrase.length() - (i + 1))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
---
+++ src/test/java/org/project/example/PalindromeTest.java
@@ -0,0 +1,26 @@
+package org.project.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromeTest {
+
+	@Test
+	void testIsPalindrome() {
+		
+		String test1true = "Able was I ere I saw Elba";
+		String test2true = "A man, a plan, a canal – Panama";
+		String test3false = "This is fake";
+		String test4false = "This is false";
+		
+		Palindrome palindromw = new Palindrome();
+		
+		assertTrue(palindromw.isPalindrome(test1true));
+		assertTrue(palindromw.isPalindrome(test2true));
+		assertFalse(palindromw.isPalindrome(test3false));
+		assertFalse(palindromw.isPalindrome(test4false));
+				
+	}
+
+}
